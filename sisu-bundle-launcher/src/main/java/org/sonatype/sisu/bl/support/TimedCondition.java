e2a927298703cceaea56514d4605f3ab5d814ecb
Alin Dreghiciu <adreghiciu@gmail.com>
2012-08-08 16:15:15 +0300

    javaDoc
    Signed-off-by: Alin Dreghiciu <adreghiciu@gmail.com>

--- sisu-bundle-launcher/src/main/java/org/sonatype/sisu/bl/support/TimedCondition.java
+++ sisu-bundle-launcher/src/main/java/org/sonatype/sisu/bl/support/TimedCondition.java
@@ -15,13 +15,29 @@ package org.sonatype.sisu.bl.support;
 /**
  * Waits for a condition to become satisfied in a timed manner.
  *
- * @since 1.0
+ * @since 1.3
  */
 public abstract class TimedCondition
 {
 
-    protected abstract boolean isSatisfied() throws Exception;
+    /**
+     * When the condition becomes satisfied.
+     *
+     * @return true if condition is satisfied
+     * @throws Exception if evaluating the condition failed. It will be ignored (and considered that condition was not
+     *                   satisfied)
+     */
+    protected abstract boolean isSatisfied()
+        throws Exception;
 
+    /**
+     * Waits for this condition to become satisfied.
+     *
+     * @param delay    before first check of condition (milliseconds)
+     * @param timeout  after which condition is considered unsatisfied (milliseconds)
+     * @param interval between checks that condition is satisfied (milliseconds)
+     * @return true if condition was satisfied before the specified timeout
+     */
     public boolean await( final long delay, final long timeout, final long interval )
     {
         sleep( delay );
@@ -44,6 +60,34 @@ public abstract class TimedCondition
         return false;
     }
 
+    /**
+     * Waits for this condition to become satisfied without a delay.
+     *
+     * @param timeout  after which condition is considered unsatisfied (milliseconds)
+     * @param interval between checks that condition is satisfied (milliseconds)
+     * @return true if condition was satisfied before the specified timeout
+     */
+    public boolean await( final long timeout, final long interval )
+    {
+        return await( 0, timeout, interval );
+    }
+
+    /**
+     * Waits for this condition to become satisfied without a delay, checking the condition at each second.
+     *
+     * @param timeout after which condition is considered unsatisfied (milliseconds)
+     * @return true if condition was satisfied before the specified timeout
+     */
+    public boolean await( final long timeout )
+    {
+        return await( 0, timeout, 1000 );
+    }
+
+    /**
+     * Sleep for specified number of milliseconds.
+     *
+     * @param delay number of milliseconds to sleep
+     */
     protected void sleep( final long delay )
     {
         if ( delay > 0 )
@@ -57,16 +101,6 @@ public abstract class TimedCondition
                 // ignore
             }
         }
-    }
-
-    public boolean await( final long timeout, final long interval )
-    {
-        return await( 0, timeout, interval );
-    }
-
-    public boolean await( final long timeout )
-    {
-        return await( 0, timeout, 1000 );
     }
 
 }
